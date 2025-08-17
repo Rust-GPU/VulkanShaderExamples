39d98fa617cf23f5c47d6d61badf4c1e5c5490bc
Christian Legnitto <christian@legnitto.com>
2025-08-16 17:37:47 -0700

    Update rust shaders to work with latest upstream main

--- shaders/rust/offscreen/phong/src/lib.rs
+++ shaders/rust/offscreen/phong/src/lib.rs
@@ -1,7 +1,11 @@
 #![cfg_attr(target_arch = "spirv", no_std)]
 #![allow(clippy::missing_safety_doc)]
 
-use spirv_std::{spirv, glam::{vec4, Mat4, Vec3, Vec4}, num_traits::Float};
+use spirv_std::{
+    glam::{vec4, Mat4, Vec3, Vec4},
+    num_traits::Float,
+    spirv,
+};
 
 #[repr(C)]
 #[derive(Copy, Clone)]
@@ -31,9 +35,9 @@ pub fn main_vs(
     let eye_pos = ubo.view * ubo.model * vec4(in_pos.x, in_pos.y, in_pos.z, 1.0);
     *out_eye_pos = eye_pos.truncate();
     *out_light_vec = (ubo.light_pos.truncate() - *out_eye_pos).normalize();
-    
+
     // Clip against reflection plane
-    let clip_plane = vec4(0.0, -1.0, 0.0, 0.0);
+    let clip_plane = vec4(0.0, 0.0, 0.0, 0.0);
     clip_distance[0] = vec4(in_pos.x, in_pos.y, in_pos.z, 1.0).dot(clip_plane);
 }
 
@@ -47,16 +51,21 @@ pub fn main_fs(
 ) {
     let eye = (-in_eye_pos).normalize();
     let reflected = (-in_light_vec).reflect(in_normal).normalize();
-    
+
     let i_ambient = vec4(0.1, 0.1, 0.1, 1.0);
-    let i_diffuse = vec4(in_normal.dot(in_light_vec).max(0.0), in_normal.dot(in_light_vec).max(0.0), in_normal.dot(in_light_vec).max(0.0), in_normal.dot(in_light_vec).max(0.0));
+    let i_diffuse = vec4(
+        in_normal.dot(in_light_vec).max(0.0),
+        in_normal.dot(in_light_vec).max(0.0),
+        in_normal.dot(in_light_vec).max(0.0),
+        in_normal.dot(in_light_vec).max(0.0),
+    );
     let specular = 0.75;
     let mut i_specular = vec4(0.0, 0.0, 0.0, 0.0);
     if in_eye_pos.dot(in_normal) < 0.0 {
         let spec_factor = reflected.dot(eye).max(0.0).powf(16.0) * specular;
         i_specular = vec4(0.5, 0.5, 0.5, 1.0) * spec_factor;
     }
-    
+
     let color_vec4 = vec4(in_color.x, in_color.y, in_color.z, 1.0);
     *out_frag_color = (i_ambient + i_diffuse) * color_vec4 + i_specular;
 }
